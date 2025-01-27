017b30559d4f9e97849a9b32e161cdae64ca5f62
Yehor <bonka335egorka@gmail.com>
2025-01-27 22:28:45 +0100

    Add lesson 5

---
+++ front-end-138/html-css/05-lesson/lesson/index.js
@@ -0,0 +1,28 @@
+// мутод get используеться для передачи малого количества информации, в то время как post для передачи большого количества информации включая файлы, также с post надо использовать multipart/form-data
+//для всех тегов кроче тега form можно применить атрибут disable
+// tabindex применятся для выстраение цепочки переходов при нажатии клавиши Tab, для груп tabindex один
+//  <form action="#" method="post" enctype="multipart/form-data"></form>
+//           <input placeholder="tu pisat nado" type="text" name="username" value="" />
+//           <input type="password" name="userpass" value="" />
+//           <p>
+//             <input checked type="radio" name="beer" value="lager" /> светлое
+//           </p>
+//           <p><input type="radio" name="beer" value="dark" /> темное</p>
+//         </div>
+//         <p><input type="checkbox" name="Car" value="yes" /> наличие авто</p>
+//         <p><input type="checkbox" name="Car2" value="yes" /> наличие авто2</p>
+//         <p><input type="checkbox" name="Car3" value="yes" /> наличие авто3</p>
+//         <input type="file" name="foto" />
+//         <div>
+//           <textarea placeholder="tut pisat" name="text"></textarea>
+//         </div>
+//         <div>
+//           <select name="color">
+//             <option  value="red">красный</option>
+//             <option selected value="yellow">желтый</optionselected>
+//             <option value="blue">голубой</option>
+//             multiple дает возможность выбрать несколько елементов
+//           </select>
+// 					<button type="submit">отправить все поля</button>
+// 					<button type="reset"> очистить все</button>
+//         </div>
